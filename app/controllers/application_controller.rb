dd786abc095a1999ed089e4e38df2f7f54a9c083
Rebecca Tong <rtong@buffalo.edu>
2018-07-16 18:01:34 +0000

    finished some of it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,27 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
   end
+
+  get '/posts/new' do
+  erb :new
+  end
+
+  post '/posts' do
+    @name = params[:name]
+    @content = params[:content]
+    @posts = Post.all.count
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+  @post = Post.find(params[:id])
+  erb :show
+  end
+
 end
