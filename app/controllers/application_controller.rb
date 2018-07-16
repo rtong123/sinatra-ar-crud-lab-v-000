cb9404e5e349c0e4af427fc04e938cfae29602e6
Rebecca Tong <rtong@buffalo.edu>
2018-07-16 21:19:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
   patch '/posts/:id' do
     @post = Post.find(id: params[:id])
     @post.update(name: params[:name], content: params[:content])
-        redirect to '/posts/:id'
+        redirect to '/posts/#{params[:id]}'
   end
 
 end
